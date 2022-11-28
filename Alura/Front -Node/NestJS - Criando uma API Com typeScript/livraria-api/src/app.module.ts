34b0c72f13696ac1b57fe25a95925d6ead9b7101
Lucas Henrique de Souza <55006206+lucasdesouza-dev@users.noreply.github.com>
2022-11-28 12:16:41 -0300

    trabalho

---
+++ Alura/Front -Node/NestJS - Criando uma API Com typeScript/livraria-api/src/app.module.ts
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { ConfigModule } from '@nestjs/config'
+import { Livro } from './livro.model';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: process.env.USUARIO_BANCO_DE_DADOS,
+      password: process.env.SENHA_BANCO_DE_DADOS,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro]),
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
+})
+export class AppModule {}
